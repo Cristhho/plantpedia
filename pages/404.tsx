743dd431d4a9b12386cbcce0cd985b5994613df4
cristhian ochoa <cristhho@gmail.com>
2021-08-24 12:32:13 -0500

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import { Layout } from "@components/Layout"
+import { Typography } from "@ui/Typography"
+import { Button } from '@ui/Button'
+
+export default function NotFoundPage() {
+  return (
+    <Layout title='404'>
+      <div className='text-center'>
+        <Typography variant='h2' className='mb-6'>🍂 We are sorry</Typography>
+        <Typography variant='h2' className='mb-6'>We couldn't find what you were looking for</Typography>
+        <Button color='primary' variant='contained' href='/' title='Go back home'>Go back home</Button>
+      </div>
+    </Layout>
+  )
+}
